3a6529759151e10890ccb4a1692d65063d8be3f6
EM <eve.proj.fun@gmail.com>
2023-02-04 16:03:55 +0800

    try integration test basic setup 2

---
+++ try_integration_test/tests/common.rs
@@ -0,0 +1,7 @@
+pub fn function_1() {
+    println!("Running function_1");
+}
+
+pub fn function_2() {
+    println!("Running function_2");
+}
--- try_integration_test/tests/sum.rs
+++ try_integration_test/tests/sum.rs
@@ -1,6 +1,18 @@
 use try_integration_test::sum;
 
+mod common;
+use common::{function_1,function_2};
+
 #[test]
 fn sum_test() {
-    assert_eq!(sum(4,6),10);
+    assert_eq!(sum(4, 6), 10);
+}
+
+// cargo test -> without println!(XXX) result
+// cargo test -- --nocapture -> with println!(XXX) result
+#[test]
+fn common_test() {
+    function_1();
+    assert_eq!(sum(8, 3), 11);
+    function_2();
 }
